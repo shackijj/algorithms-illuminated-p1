2c8a58b8880a65cdce0020642d3382ff5d12f32f
Kirill Shatskiy <shaackiy@gmail.com>
2018-07-18 13:15:18 +0300

    feat: implemement the simplest version of mergeSort and add tests for that

--- src/mergeSort.ts
+++ src/mergeSort.ts
@@ -1,31 +1,46 @@
+function merge(sortedA: number[], sortedB: number[]) {
+    const result = [];
+    const length = sortedA.length + sortedB.length;
+    for (let i = 0, j = 0, k = 0; i < length; i++) {
+        if (sortedA[j] >= sortedB[k]) {
+            result[i] = sortedB[k];
+            k++;
+            if (k === sortedB.length) {
+                return result.concat(sortedA.slice(j, sortedA.length))
+            }
+        } else {
+            result[i] = sortedA[j];
+            j++;
+            if (j === sortedA.length) {
+                return result.concat(sortedB.slice(k, sortedB.length))
+            }
+        }
+    }
+    return result;
+}
+
 export default function mergeSort(input: number[]): number[] {
     const length = input.length;
-    if (length === 2) {
+    if (length === 1) {
+        return input.concat([]);
+    } else if (length === 2) {
         if (input[0] > input[1]) {
             return [input[1], input[0]];
         } else {
             return [input[0], input[1]];
         }
     } else {
-        const result = [];
-        let unprimed = length % 2 === 0 ? null : input[length - 1];
-        const a = unprimed === null ? input.slice(0, length / 2) : input.splice(0, (length - 1) / 2);
-        const b = unprimed === null ? input.slice(length / 2, length) : input.splice((length - 1) / 2, length - 1);
-    
+        let unprimed = length % 2 === 0 ? null : [input[length - 1]];
+        const a = unprimed === null ? input.slice(0, length / 2) : input.slice(0, (length - 1) / 2);
+        const b = unprimed === null ? input.slice(length / 2, length) : input.slice((length - 1) / 2, length - 1);
+ 
         const sortedA = mergeSort(a);
         const sortedB = mergeSort(b);
-        for (let i = 0, j = 0, k = 0; i < length; i++) {
-            if (unprimed !== null && sortedA[j] >= unprimed && sortedB[k] >= unprimed) {
-                result[i] = unprimed;
-                unprimed = null;
-            } else if (sortedA[j] >= sortedB[k]) {
-                result[i] = sortedB[k];
-                k++;
-            } else {
-                result[i] = sortedA[j];
-                j++;
-            }
+        const result = merge(sortedA, sortedB);
+        if (unprimed) {
+            return merge(result, unprimed);
+        } else {
+            return result;
         }
-        return result;
     }
 }
--- test/mergeSort.spec.ts
+++ test/mergeSort.spec.ts
@@ -15,6 +15,10 @@ describe('mergeSort', () => {
             output: [1, 2, 3, 4]
         },
         {
+            input: [5, 1, 7],
+            output: [1, 5, 7]
+        },
+        {
             input: [5, 1, 7, 2, 6, 2],
             output: [1, 2, 2, 5, 6, 7]
         }
