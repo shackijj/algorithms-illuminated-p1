f2e77aa53a49598b6d7685a4180fbc638918c0df
Kirill Shatskiy <shaackiy@gmail.com>
2018-07-24 11:52:23 +0300

    feat: add unit tests for countInversions

--- test/countInversions.spec.ts
+++ test/countInversions.spec.ts
@@ -4,7 +4,7 @@ describe('countInversions', () => {
     function checkOutput({input, output}: {input: number[], output: TInversions}) {
         describe(`given that input is ${JSON.stringify(input)}`, () => {
             it('should count number of inversions', () => {
-                expect(countInversions(input)).toBe(output);
+                expect(countInversions(input)).toEqual(output);
             });
         })
     }
@@ -12,6 +12,14 @@ describe('countInversions', () => {
         {
             input: [5, 4, 1, 3],
             output: {array: [1, 3, 4, 5], count: 5}
+        },
+        {
+            input: [5, 4, 1, 3, 9, 12, 23, 14],
+            output: {array: [1, 3, 4, 5, 9, 12, 14, 23], count: 6}
+        },
+        {
+            input: [5, 4, 3, 1],
+            output: {array: [1, 3, 4, 5], count: 6}
         },
     ].forEach(checkOutput);
 });
