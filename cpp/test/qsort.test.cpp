949d0c987d3bf03dfb57607f437fd2afd6890169
Kirill Shatskiy <shaackiy@gmail.com>
2018-08-29 11:22:12 +0300

    feat: add counting the number of comparissons

--- cpp/test/qsort.test.cpp
+++ cpp/test/qsort.test.cpp
@@ -3,7 +3,17 @@
 #include <qsort.hpp>
 
 TEST(qsortTest, ArrayWithDescentElement) {
-    int array[] = {3, 2, 1};
-    qsort(array, 3);
-    ASSERT_THAT(array, testing::ElementsAre(1, 2, 3));
+    int array1[] = {1, 2, 3};
+    ASSERT_EQ(qsort(array1, 3), 3);
+    ASSERT_THAT(array1, testing::ElementsAre(1, 2, 3));
+
+    int array2[] = {3, 2, 1, 5};
+    ASSERT_EQ(qsort(array2, 4), 4);
+    ASSERT_THAT(array2, testing::ElementsAre(1, 2, 3, 5));
+}
+
+TEST(qsortTest, ArrayWithRepeatingElement) {
+    int array[] = {3, 2, 2, 1, 5};
+    qsort(array, 5);
+    ASSERT_THAT(array, testing::ElementsAre(1, 2, 2, 3, 5));
 }
