19a8e7c4123a3e9439659c2a335db7ecd3d33acb
Kirill Shatskiy <shaackiy@gmail.com>
2018-10-11 11:26:02 +0300

    feat: implement edge contracting

--- js/src/kargerMinCut.ts
+++ js/src/kargerMinCut.ts
@@ -1,23 +1,69 @@
-type Edge = number[];
-type Vertex = number[];
+type Edge = number[] | null;
+type Vertex = number[] | null;
 
 class AdjList {
     private edges: Edge[];
     private vertices: Vertex[];
-
+    private edgeCount: number;
+    private verticesCount: number;
     constructor(str: string) {
         this.edges = [];
         this.vertices = [];
+        this.edgeCount = 0;
+        this.verticesCount = 0;
         this._parseGraphString(str);
     }
     public toString(): string {
         const toJson = JSON.stringify;
+        this.vertices.forEach(this._sortIfNotNull)
+        this.edges.forEach(this._sortIfNotNull)
         return `vertices: ${toJson(this.vertices)} edges: ${toJson(this.edges)}`;
     }
+    public getEdgeCount(): number {
+        return this.edgeCount;
+    }
+    public getVerticesCount(): number {
+        return this.verticesCount;
+    }
+    public contractEdge(idx: number) {
+        if (this.edges[idx] === null) {
+            throw new Error('Trying to contract a deleted edge');
+        }
+        const [v1num, v2num] = this.edges[idx] as number[];
+        this.edges[idx] = null;
+        const deletedEdgeNum = idx + 1;
+        this.edgeCount--;
+        const v1idx = v1num - 1;
+        const v2idx = v2num - 1;
+        // here should be a security check
+        const v1 = this.vertices[v1idx] as number[];
+        const v2 = this.vertices[v2idx] as number[];
+        const superVertex = v1.concat(v2).filter((edge) => deletedEdgeNum !== edge);
+        this.vertices[v2idx] = null;
+        this.vertices[v1idx] = superVertex;
+        this.verticesCount--;
+
+        for (let edgeNum of v2) {
+            if (edgeNum !== deletedEdgeNum) {
+                const edge = this.edges[edgeNum - 1] as number[];
+                const oldVertexIndex = edge.findIndex(vertexNum => vertexNum === v2num);
+                edge[oldVertexIndex] = v1num;
+            }
+        }
+        // merge edges for the v1
+    }
+    private _sortIfNotNull(graphEl: Edge | Vertex) {
+        if (graphEl !== null) {
+            graphEl.sort()
+        }
+    }
     private _areVerticesConnected(v1num: number, v2num: number): boolean {
-        const v1 = this.vertices[v1num - 1];
+        if (this.vertices[v1num - 1] === null || this.vertices[v2num - 1] === null) {
+            throw new Error('Trying to check a deleted vertex');
+        }
+        const v1 = this.vertices[v1num - 1] as number[];
         for (let edgeNum of v1) {
-            const edge = this.edges[edgeNum - 1];
+            const edge = this.edges[edgeNum - 1] as number[];
             if (edge.indexOf(v2num) > -1) {
                 return true;
             }
@@ -37,16 +83,19 @@ class AdjList {
         if (vertex === undefined) {
             vertex = [];
             this.vertices[vertexIdx] = vertex;
+            this.verticesCount++;
         }
 
         for (let adjVertexNum of adjacentVertices) {
             const adjVertexIdx = adjVertexNum - 1;
             let adjVertex = this.vertices[adjVertexIdx];
             if (!adjVertex) {
+                this.verticesCount++;
                 adjVertex = [];
                 this.vertices[adjVertexIdx] = adjVertex;
             }
             if (!this._areVerticesConnected(vertexNum, adjVertexNum)) {
+                this.edgeCount++;
                 let newEdgeIdx = this.edges.length;
                 let newEdgeNum = newEdgeIdx + 1;
                 let newEdge = [vertexNum, adjVertexNum];
--- js/test/kargerMinCut.spec.ts
+++ js/test/kargerMinCut.spec.ts
@@ -1,15 +1,74 @@
 import {AdjList} from '../src/kargerMinCut';
 
 describe('AdjList', () => {
-    it('takes a string', () => {
+    let adjList: AdjList;
+
+    beforeEach(() => {
         const input = [
             '1 2 3',
             '2 3 1',
             '3 1 2'
         ].join('\n');
 
-        expect((new AdjList(input)).toString()).toEqual(
-            'vertices: [[1,2],[1,3],[2,3]] edges: [[1,2],[1,3],[2,3]]');
+        adjList = new AdjList(input);
+    });
+
+    describe('#toString', () => {
+        it('shold return an array of vertices and an array of edges', () => {
+            expect(adjList.toString()).toEqual(
+                'vertices: [[1,2],[1,3],[2,3]] edges: [[1,2],[1,3],[2,3]]');
+        });
+    });
+
+    describe('#getEdgeCount', () => {
+        it('shold return an array of vertices and an array of edges', () => {
+            expect(adjList.getEdgeCount()).toEqual(3);
+        });
+    });
+
+    describe('#getVerticesCount', () => {
+        it('shold return an array of vertices and an array of edges', () => {
+            expect(adjList.getVerticesCount()).toEqual(3);
+        });
+    });
+
+    describe('#contractEdge', () => {
+        describe('given that 1st edge is contracted', () => {
+            beforeEach(() => {
+                adjList.contractEdge(0);
+            });
+
+            it('should merge vertives 1 and 2 and delete self-loops', () => {
+                expect(adjList.toString()).toEqual(
+                    'vertices: [[2,3],null,[2,3]] edges: [null,[1,3],[1,3]]');
+            });
+
+            it('should have 2 vertices', () => {
+                expect(adjList.getVerticesCount()).toEqual(2);
+            });
+
+            it('should have 2 edges', () => {
+                expect(adjList.getEdgeCount()).toEqual(2);
+            });
+        });
+        describe('given that 2nd edge is contracted', () => {
+            beforeEach(() => {
+                adjList.contractEdge(1);
+            });
+
+            it('should merge vertives 1 and 2 and delete self-loops', () => {
+                expect(adjList.toString()).toEqual(
+                    'vertices: [[1,3],[1,3],null] edges: [[1,2],null,[1,2]]');
+            });
+
+            it('should have 2 vertices', () => {
+                expect(adjList.getVerticesCount()).toEqual(2);
+            });
+
+            it('should have 2 edges', () => {
+                expect(adjList.getEdgeCount()).toEqual(2);
+            });
+        });
     });
 });
 
